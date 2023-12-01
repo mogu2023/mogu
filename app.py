f71493e04c654342495e35548bdf73eda9841422
mogu <mogu@qq.com>
2023-12-01 22:02:14 +0800

    周五晚上

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('hello word')
+import dash
+from dash import html
+from sidebar import SideBarView
+
+external_script = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
+
+app = dash.Dash(__name__,
+                external_scripts=external_script)
+app.scripts.config.serve_locally = True
+
+app.layout = html.Div(
+    SideBarView()
+)
+
+app.run(debug=True)
